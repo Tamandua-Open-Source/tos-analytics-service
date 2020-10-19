f751bafcfee6799dff6d310b7886c561685e39b8
Otávio Baziewicz Filho <otavio.baziewicz.filho@gmail.com>
2020-10-19 13:15:39 -0300

    Add cycle route on infrastructure layer

--- api/lib/infrastructure/repositories/analytics-repository.js
+++ api/lib/infrastructure/repositories/analytics-repository.js
@@ -1,5 +1,6 @@
 import db from '../orm/models'
 import IAnalyticsRepository from '../../application/repository-interfaces/i-analytics-repository'
+import { Op } from 'sequelize'
 
 class AnalyticsRepository extends IAnalyticsRepository {
   async getAllUserActions() {
@@ -18,6 +19,24 @@ class AnalyticsRepository extends IAnalyticsRepository {
     return await db.UserAction.findAll({
       where: {
         UserId: userId,
+      },
+      attributes: ['UserId', 'createdAt'],
+      include: [
+        {
+          model: db.Action,
+          attributes: ['id', 'name'],
+        },
+      ],
+    })
+  }
+
+  async getUserActionsBetween(userId, startDate, endDate) {
+    return await db.UserAction.findAll({
+      where: {
+        UserId: userId,
+        createdAt: {
+          [Op.between]: [startDate, endDate],
+        },
       },
       attributes: ['UserId', 'createdAt'],
       include: [
